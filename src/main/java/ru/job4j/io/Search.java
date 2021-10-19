3b33c19b6cda894b5c990175632aaaae3e822b31
Stikkerrr <stikkerrrr@gmail.com>
2021-10-19 11:51:13 +0300

    5. Валидация параметров запуска. #246865 #217968

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,13 +8,27 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException(String.format(
+                    "Invalid number of arguments: %n%s",
+                    "java -jar search.jar START_FOLDER EXTENSION"));
+        }
+        var start = Paths.get(args[0]);
+        if (!start.toFile().exists()) {
+            throw new IllegalArgumentException(String.format(
+                    "Directory %s does not exist", start));
+        }
+        if (!start.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format(
+                    "%s is not a directory", start));
+        }
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        SearchFiles searcher = new SearchFiles(condition);
+        var searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
